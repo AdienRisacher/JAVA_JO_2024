14b2ebfce6dd0a2fb5041448a47f0503ca4f0606
AdienRisacher <116495676+AdienRisacher@users.noreply.github.com>
2023-06-21 12:21:08 +0200

    Update Bdd.java

--- src/main/java/com/example/java_jo_2024/Bdd.java
+++ src/main/java/com/example/java_jo_2024/Bdd.java
@@ -7,7 +7,7 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class Bdd {
-    private String jdbcURL = "jdbc:mysql://root:tribadri23@localhost:3306/jo";
+    private String jdbcURL = "jdbc:mysql://root:@localhost:3306/jo";
     private String jdbcUsername = "root";
     private String jdbcPassword = "";
     private Connection jdbcConnexion;
